e4c9a95e7956a7c2c8c0e76d49552daa1e99bdde
Oemer Genc <oemergenc@gmail.com>
2019-03-31 09:18:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --branch feature/bare-git --separate-git-dir=$HOME/.dotfiles https://github.com/oemergenc/dotfiles.git $HOME/tmpdotfiles
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/oemergenc/dotfiles.git $HOME/tmpdotfiles
 
 chmod +x $HOME/tmpdotfiles/.dot/install/setup-dotfiles.sh
 $HOME/tmpdotfiles/.dot/install/setup-dotfiles.sh
